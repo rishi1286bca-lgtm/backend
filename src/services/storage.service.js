ad15b4ff62e485c70221bc5854e9a64a5af064a4
rishi1286bca-lgtm <rishi1286bca@gmail.com>
2026-05-20 15:16:41 +0530

    fixed cors and backend errors

--- src/services/storage.service.js
+++ src/services/storage.service.js
@@ -1,8 +1,9 @@
 const ImageKit = require('@imagekit/nodejs')
 
 const imagekit = new ImageKit({
-
-    privateKey: "private_uvmEqvs86AVA3U6prpDwDnmnf6Y="
+    publicKey: "public_BDAWc9203bR5cC6K5Y8pd1opOYU=",
+    privateKey: "private_uvmEqvs86AVA3U6prpDwDnmnf6Y=",
+    urlendpoint: "https://ik.imagekit.io/wb2p817u3"
 })
 
 async function uploadFile(buffer) {

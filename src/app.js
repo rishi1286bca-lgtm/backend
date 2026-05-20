6ba877f8c38186cf78e4bf7a8d8b1060702d9643
rishi1286bca-lgtm <rishi1286bca@gmail.com>
2026-05-20 14:51:57 +0530

    fixed cors and backend errors

--- src/app.js
+++ src/app.js
@@ -6,7 +6,11 @@ const cors = require("cors")
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "https://post-frontenf.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 const uplaod = multer({storage: multer.memoryStorage()})
 
 
